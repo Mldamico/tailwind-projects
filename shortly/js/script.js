242ede36e7d1a6d414e73616baa7b1e0d1942a04
mldamico <matias.lioneldamico@gmail.com>
2022-08-19 19:40:50 -0300

    basic js

---
+++ shortly/js/script.js
@@ -0,0 +1,34 @@
+const errMsg = document.getElementById('err-msg')
+const input = document.getElementById('link-input')
+const linkForm = document.getElementById('link-form')
+
+function validURL(str) {
+  var pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+    '((\\d{1,3}\\.){3}\\d{1,3}))' +
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+    '(\\?[;&a-z\\d%_.~+=-]*)?' +
+    '(\\#[-a-z\\d_]*)?$',
+    'i'
+  )
+  return !!pattern.test(str)
+}
+
+
+const formSubmit = (e) => {
+
+  e.preventDefault()
+  if (input.value === '') {
+    errMsg.innerHTML = "Cannot be empty. Please enter an URL."
+    input.classList.add('border-red')
+  } else if (!validURL(input.value)) {
+    errMsg.innerHTML = "Please enter a valid URL"
+    input.classList.add('border-red')
+  } else {
+    input.classList.remove('border-red')
+    alert("Success")
+  }
+}
+
+linkForm.addEventListener('submit', formSubmit)
